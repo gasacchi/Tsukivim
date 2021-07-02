a06fc77367e1186f6b65bb4aace73d18812fa375
gasacchi <6asacchi@gmail.com>
2021-07-02 22:04:46 +0700

    fix warning

--- lua/modules/utils/telescope.lua
+++ lua/modules/utils/telescope.lua
@@ -9,7 +9,6 @@ require('telescope').setup{
       '--column',
       '--smart-case'
     },
-    prompt_position = "bottom",
     prompt_prefix = " λ ",
     selection_caret = "> ",
     entry_prefix = "  ",
@@ -17,7 +16,7 @@ require('telescope').setup{
     selection_strategy = "reset",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
-    layout_defaults = {
+    layout_config = {
       horizontal = {
         mirror = false,
       },
@@ -30,13 +29,8 @@ require('telescope').setup{
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     shorten_path = true,
     winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-    -- borderchars = { "═", "│", "═", "│", "╒", "╕", "╛", "╘" },
     color_devicons = true,
     use_less = true,
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,

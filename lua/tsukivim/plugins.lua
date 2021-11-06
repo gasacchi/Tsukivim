15c2ff79e8b8250629333ad1a8fa53e505f35180
gasacchi <6asacchi@gmail.com>
2021-11-06 16:29:59 +0700

    remove unused plugin and cleaning

--- lua/tsukivim/plugins.lua
+++ lua/tsukivim/plugins.lua
@@ -5,9 +5,9 @@ return require'packer'.startup(function(use)
   use 'wbthomason/packer.nvim'                 -- Plugin Manager
   -----------------------------------------------------------------------------
 
+
   -- LSP & Completion --------------------------------------------------------
   use 'neovim/nvim-lspconfig'                  -- Config quickstart
-
   use 'onsails/lspkind-nvim'                   -- LSP kind icons like vscode
   use {                                        -- nvim completion
     "hrsh7th/nvim-cmp",
@@ -20,33 +20,28 @@ return require'packer'.startup(function(use)
       "hrsh7th/cmp-nvim-lua"
     }
   }
-
-
-
-
-  use 'kosayoda/nvim-lightbulb'                -- Lightbulb 
+  use 'kosayoda/nvim-lightbulb'                -- Lightbulb
   use 'folke/trouble.nvim'                     -- Pretty Diagnostics
-  -- NOTE: will use for until glepnir come back
-  -- use 'glepnir/lspsaga.nvim'                   -- LSP UI 
   use { 'tami5/lspsaga.nvim', branch = 'nvim51' }
   -----------------------------------------------------------------------------
 
+
   -- Git ----------------------------------------------------------------------
-  use 'TimUntersberger/neogit'                 -- Magit for Neovim 
-  use 'lewis6991/gitsigns.nvim'                -- Git Sign 
-  use 'f-person/git-blame.nvim'                -- Show Git Blame 
-  use 'kdheepak/lazygit.nvim'                  -- Lazygit wrapper
+  use 'TimUntersberger/neogit'                 -- Magit for Neovim
+  use 'lewis6991/gitsigns.nvim'                -- Git Sign
+  use 'f-person/git-blame.nvim'                -- Show Git Blame
   -----------------------------------------------------------------------------
+
 
   -- Editing ------------------------------------------------------------------
   use 'b3nj5m1n/kommentary'                    -- Code Comment 
   use 'tpope/vim-surround'                     -- Surround
-  use 'phaazon/hop.nvim'                       -- Jump / Hop Motion 
-  -- use 'vhyrro/neorg'                           -- Org mode in neovim
+  use 'phaazon/hop.nvim'                       -- Jump / Hop Motion
   -----------------------------------------------------------------------------
 
+
   -- Utility ------------------------------------------------------------------
-  use {                                        -- Fuzzy Finder 
+  use {                                        -- Fuzzy Finder
     'nvim-telescope/telescope.nvim',
     requires = {
       {'nvim-lua/popup.nvim'},
@@ -57,8 +52,10 @@ return require'packer'.startup(function(use)
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons'
   }
-  use 'folke/todo-comments.nvim'               -- PERF utile
+  use 'folke/todo-comments.nvim'               -- Todo Comment
+  use 'akinsho/toggleterm.nvim'
   -----------------------------------------------------------------------------
+
 
   -- UI -----------------------------------------------------------------------
   use 'nvim-treesitter/nvim-treesitter'
@@ -69,18 +66,14 @@ return require'packer'.startup(function(use)
     'glepnir/galaxyline.nvim',
     branch = 'main'
   }
-  use {                                        -- Indent Guide
-    'lukas-reineke/indent-blankline.nvim',
-    -- branch = 'lua'
-  }
+  use 'lukas-reineke/indent-blankline.nvim'    -- Indent Guide
   use 'folke/tokyonight.nvim'
   use 'glepnir/dashboard-nvim'                 -- Fancy Dashboard
-  use 'akinsho/nvim-bufferline.lua'            -- Bufferline 
-  use 'norcalli/nvim-colorizer.lua'            -- HEX Colorizer 
-  use 'wfxr/minimap.vim'                       -- blazing fast minimap
-  -- use 'psliwka/vim-smoothie'                   -- Smooth scrolling
-  use 'junegunn/goyo.vim'                      -- Zen Mode
+  -- use 'goolord/alpha-nvim'                  -- Fancy Dashboard
+  use 'akinsho/bufferline.nvim'                -- Bufferline
+  use 'norcalli/nvim-colorizer.lua'            -- HEX Colorizer
   -----------------------------------------------------------------------------
+
 
   -- Language, etc. -----------------------------------------------------------
   use 'sheerun/vim-polyglot'                   -- Polyglot Syntax Highlight

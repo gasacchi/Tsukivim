2fafe1e5e4cea89382443a0543dd1bf6d5cad80c
gasacchi <6asacchi@gmail.com>
2021-11-06 20:46:24 +0700

    add toggleterm

--- lua/tsukivim.lua
+++ lua/tsukivim.lua
@@ -26,6 +26,7 @@ function M.init ()
   require'modules.utils.nvimtree'
   require'modules.utils.telescope'
   require'modules.utils.todo-comments'
+  require'modules.utils.toggleterm'
   -- require'modules.utils.tabout'
 
   -- ui
